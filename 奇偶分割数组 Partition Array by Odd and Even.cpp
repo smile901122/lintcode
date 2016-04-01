373461de4c439737487105727ded9f02bf752c59
momosmile <443106364@qq.com>
2016-04-01 16:45:03 +0800

    Create 奇偶分割数组 Partition Array by Odd and Even.cpp

---
+++ 奇偶分割数组 Partition Array by Odd and Even.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @return: nothing
+     */
+    void partitionArray(vector<int> &nums) {
+        // write your code here
+        if(nums.empty())    return;
+        int i = 0;
+        int j = nums.size() - 1;
+        while(i < j)
+        {
+            while(i < j && nums[i] % 2 == 1)    ++i;
+            while(i < j && nums[j] % 2 == 0)    --j;
+            if(i > j)   break;
+            swap(nums[i], nums[j]);
+        }
+        return;
+    }
+};
