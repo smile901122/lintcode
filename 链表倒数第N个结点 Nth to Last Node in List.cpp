07a26c9c72dac6694468872af7c08cd84820204e
momosmile <443106364@qq.com>
2016-04-01 16:27:02 +0800

    Create 链表倒数第N个结点 Nth to Last Node in List.cpp

---
+++ 链表倒数第N个结点 Nth to Last Node in List.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: Nth to last node of a singly linked list. 
+     */
+    ListNode *nthToLast(ListNode *head, int n) {
+        // write your code here
+        /*
+        if(head == NULL)    return head;
+        ListNode *p1 = head;
+        ListNode *p2 = head;
+        while(p1->next != NULL && n > 1)
+        {
+            p1 = p1->next;
+            --n;
+        }
+        while(p1->next != NULL)
+        {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p2;
+        */
+        ListNode *p1, *p2;
+        p1 = head;
+        while(n > 0)
+        {
+            p1 = p1->next;
+            --n;
+        }
+        p2 = head;
+        while (p1 != NULL) {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p2;
+    }
+};
+
+
