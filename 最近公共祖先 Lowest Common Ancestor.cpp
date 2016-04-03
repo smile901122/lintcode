362fd0b8aa0a66f3f96a6f3f658c40dfe362d1d9
momosmile <443106364@qq.com>
2016-04-03 15:41:27 +0800

    Create 最近公共祖先 Lowest Common Ancestor.cpp

---
+++ 最近公共祖先 Lowest Common Ancestor.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *A, TreeNode *B) {
+        // write your code here
+        /*
+        if(root == NULL || root == A || root == B)  return root;
+        TreeNode *left = lowestCommonAncestor(root->left, A, B);
+        TreeNode *right = lowestCommonAncestor(root->right, A, B);
+        if(left != NULL && right != NULL)    return root;
+        if(left != NULL)    return left;
+        else if(right != NULL)   return right;
+        else return NULL;
+        */
+        if ((NULL == A) || (NULL == B)) return NULL;
+
+        pair<TreeNode *, int> result = helper(root, A, B);
+
+        if (A != B) {
+            return (2 == result.second) ? result.first : NULL;
+        } else {
+            return (1 == result.second) ? result.first : NULL;
+        }
+    }
+
+private:
+    pair<TreeNode *, int> helper(TreeNode *root, TreeNode *A, TreeNode *B) {
+        TreeNode * node = NULL;
+        if (NULL == root) return make_pair(node, 0);
+
+        pair<TreeNode *, int> left = helper(root->left, A, B);
+        pair<TreeNode *, int> right = helper(root->right, A, B);
+
+        // return either A or B
+        int count = max(left.second, right.second);
+        if (A == root || B == root)  return make_pair(root, ++count);
+
+        // A and B are on both sides
+        if (NULL != left.first && NULL != right.first) return make_pair(root, 2);
+
+        // return either left or right or NULL
+        return (NULL != left.first) ? left : right;
+    }
+};
