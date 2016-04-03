cdef32dc1fca9de0e502d72c2270636bc61e3ed8
momosmile <443106364@qq.com>
2016-04-03 17:42:22 +0800

    Create 两数之和 Two Sum.cpp

---
+++ 两数之和 Two Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1+1, index2+1] (index1 < index2)
+     */
+    vector<int> twoSum(vector<int> &nums, int target) {
+        // write your code here
+        vector<int> index;
+        unordered_map<int, int> map;
+        int gap;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            map[nums[i]] = i;
+        }
+        for(int j = 0; j < nums.size(); ++j)
+        {
+            gap = target - nums[j];
+            if(map.find(gap) != map.end() && map[gap] > j)
+            {
+                index.push_back(j + 1);
+                index.push_back(map[gap] + 1);
+                return index;
+            }
+        }
+        return index;
+    }
+};
