136109b01b8e8aa1705de587d957b9c85f32c4ed
momosmile <443106364@qq.com>
2016-04-03 17:05:43 +0800

    Create 最小差  The Smallest Difference.cpp

---
+++ 最小差  The Smallest Difference.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    /**
+     * @param A, B: Two integer arrays.
+     * @return: Their smallest difference.
+     */
+    int smallestDifference(vector<int> &A, vector<int> &B) {
+        // write your code here
+        int ret = INT_MAX;  
+        sort(B.begin(),B.end());  
+        for(int i = 0; i < A.size(); ++i)  
+        {  
+            int diff = lower_bound( B.begin(), B.end(), A[i] ) - B.begin();
+            if(diff < A.size())
+            {
+                ret = min(ret, abs(A[i] - B[diff]));
+                if(ret == 0)    return 0;
+            }
+            if(diff > 0)  
+                ret = min(ret, abs(A[i] - B[diff-1]));
+            if(ret == 0)    return 0;
+        }  
+        return ret;
+    }
+};
