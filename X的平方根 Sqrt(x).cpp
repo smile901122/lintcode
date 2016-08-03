54a1373a13b0bab1116317247003b5e77a754eab
momosmile <443106364@qq.com>
2016-08-03 09:55:50 +0800

    Update X的平方根 Sqrt(x).cpp

--- X的平方根 Sqrt(x).cpp
+++ X的平方根 Sqrt(x).cpp
@@ -6,6 +6,18 @@ public:
      */
     int sqrt(int x) {
         // write your code here
+        //method 1
+        int l = 0;
+        int r = x;
+        while(l < r){
+            int mid = (l + r + 1) / 2;
+            if(x / mid > mid)   l = mid + 1;
+            else if(x / mid == mid) return mid;
+            else    r = mid - 1;
+        }
+        return r;
+        //method 2
+        /*
         if(x < 2)   return x;
         int begin = 1;
         int end = x / 2;
@@ -29,5 +41,6 @@ public:
             }
         }
         return lastMid;
+        */
     }
 };
