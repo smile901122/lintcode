2e963b75f1c62ab802119891383b9d4af34c531b
momosmile <443106364@qq.com>
2016-08-28 21:28:04 +0800

    Update 背包问题 II Backpack II.cpp

--- 背包问题 II Backpack II.cpp
+++ 背包问题 II Backpack II.cpp
@@ -8,22 +8,17 @@ public:
     int backPackII(int m, vector<int> A, vector<int> V) {
         // write your code here
         if(A.empty() || m < 1)  return 0;
-        vector<int> bp(m + 1, -1);
         int n = A.size();
         
-        bp[0] = 0;
-        for (int i = 0; i < n; ++i) 
-        {
-            for (int j = m; j >= A[i]; --j) 
-            {
-                if (bp[j - A[i]] >= 0) 
-                    bp[j] = max(bp[j], bp[j - A[i]] + V[i]);
-            }
-        }
-        int res = 0;
-        for (auto val : bp) {
-            res = max(res, val);
-        }
-        return res;
+        vector<int> bp(m + 1, 0);
+	    for (int i = 0; i < n; ++i){
+		    for (int j = m; j >= A[i]; j--){
+			    if (bp[j - A[i]] + V[i] > bp[j]){
+				    bp[j] = bp[j - A[i]] + V[i];
+			    }
+		    }
+	    }
+
+        return bp[m];
     }
 };
