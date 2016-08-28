cfe7a2f736347cd77ab2a6d55f363ad7b5813534
momosmile <443106364@qq.com>
2016-08-28 22:17:11 +0800

    Update 背包问题 Backpack.cpp

--- 背包问题 Backpack.cpp
+++ 背包问题 Backpack.cpp
@@ -8,22 +8,16 @@ public:
     int backPack(int m, vector<int> A) {
         // write your code here
         if(A.empty() || m < 1)  return 0;
-        vector<bool> bp(m + 1, false);
-        
-        bp[0] = true;
+        vector<int> bp(m + 1, 0);
         int n = A.size();
         
-        for (int i = 0; i < n; ++i) 
-        {
-            for (int j = m; j >= A[i]; --j) 
-            {
-                if (bp[j - A[i]])   bp[j] = true;
+        for (int i = 0; i < n; ++i){
+            for (int j = m; j >= A[i]; --j){
+                if (bp[j - A[i]] + A[i] > bp[j])   
+                    bp[j] = bp[j - A[i]] + A[i];
             }
         }
-        int k = m;
-        while (!bp[k]) {
-            --k;
-        }
-        return k;
+        
+        return bp[m];
     }
 };
